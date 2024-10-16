bfbfb5bb61ac27531c198895b07e2374dc21ffa3
emilendlbb <emilien.delabrassinnebonardeaux@student.uliege.be>
2024-10-16 21:12:00 +0200

    log_method

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
+from scipy.integrate import cumulative_trapezoid
 
 current_dir = os.path.dirname(__file__)
 
@@ -21,13 +22,13 @@ def damped_natural_frequency(data_acc):
     peaks, _ = find_peaks(acc)
 
     time_peaks = time[peaks]
-    print(time_peaks)
+    # print(time_peaks)
 
     time_peaks_difference = np.diff(time_peaks)
-    print(time_peaks_difference)
+    # print(time_peaks_difference)
 
     time_peaks_difference = time_peaks_difference[7:]
-    print(time_peaks_difference)
+    # print(time_peaks_difference)
 
     avg_time_peaks_difference = np.mean(time_peaks_difference)
 
@@ -36,9 +37,33 @@ def damped_natural_frequency(data_acc):
     return damped_natural_frequency
 
 
-def log_method(data_acc):
+def integrate_acc(data_acc):
     time = data_acc[:, 0]
     acc = data_acc[:, 1]
+
+    velocity = cumulative_trapezoid(acc, time, initial=0)
+
+    displacement = cumulative_trapezoid(velocity, time, initial=0)
+
+    return displacement, velocity
+
+def log_method(displacement):
+    peaks, _ = find_peaks(displacement)
+
+    displacement_peaks = displacement[peaks]
+    print(displacement_peaks)
+
+    displacement_peaks = displacement_peaks[1:6]
+    print(displacement_peaks)
+
+    log_quotient = np.log(np.abs(displacement_peaks[:-1] / displacement_peaks[1:]))
+    print(log_quotient)
+
+    mean_log = np.mean(log_quotient)
+
+    damping_ratio = mean_log / (2 * np.pi)
+
+    return damping_ratio
 
     
 
@@ -47,6 +72,11 @@ def log_method(data_acc):
 freq = damped_natural_frequency(data_acc)
 print(freq)
 
+displacement, velocity = integrate_acc(data_acc)
+# print(displacement)
 
-plt.plot(time, acc)
+estimated_damping_ratio = log_method(displacement)
+print(estimated_damping_ratio)
+
+plt.plot(time, displacement)
 plt.show()
