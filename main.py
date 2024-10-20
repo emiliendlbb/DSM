489907d3c273c7fcb545ed15e208989c834014c1
emilendlbb <emilien.delabrassinnebonardeaux@student.uliege.be>
2024-10-20 16:16:30 +0200

    half-power method

--- main.py
+++ main.py
@@ -75,17 +75,17 @@ def log_method(displacement):
 
 
 
-natural_freq = damped_natural_frequency(data_acc)
-print(natural_freq)
+natural_freq_peaks = damped_natural_frequency(data_acc)
+print(f"Estimated natural frequency from peaks: {natural_freq_peaks} Hz")
 
-natural_omega = 2*np.pi*natural_freq
-print(natural_omega)
+natural_omega_peaks = 2*np.pi*natural_freq_peaks
+print(f"Estimated natural pulsation from peaks: {natural_omega_peaks} Hz")
 
 displacement, velocity = integrate_acc(data_acc)
 # print(displacement)
 
-estimated_damping_ratio = log_method(displacement)
-print(estimated_damping_ratio)
+damping_ratio_log_method = log_method(displacement)
+print(f"Estimated natural frequency from Bode plot: {damping_ratio_log_method} Hz")
 
 plt.plot(time, displacement)
 plt.show()
@@ -118,13 +118,33 @@ plt.ylabel("Phase (radians)")
 plt.tight_layout()
 plt.show()
 
+# natural frequency at the resonance peak
+peak_idx = np.argmax(amplitude)
+natural_frequency_bode = freq[peak_idx]
+
+print(f"Estimated natural frequency from Bode plot: {natural_frequency_bode} Hz")
+
+#half-power method
+half_power_amplitude = amplitude[peak_idx] / np.sqrt(2)
+half_power_indices = np.where(amplitude >= half_power_amplitude)[0]
+bandwidth_freqs = freq[half_power_indices]
+
+delta_f = bandwidth_freqs[-1] - bandwidth_freqs[0]
+print(f"Bandwidth Δf: {delta_f} Hz")
+
+Q_factor = natural_frequency_bode / delta_f
+print(f"Estimated Quality Factor (Q): {Q_factor}")
+
+damping_ratio_half_power = 1 / (2 * Q_factor)
+print(f"Estimated Damping Ratio using Half-Power Method: {damping_ratio_half_power}")
+
 
 #nyquist
 
 # Tracer le diagramme de Nyquist
 plt.figure()
 
-plt.plot(Re_FRF/(natural_omega)**2, Im_FRF/(natural_omega)**2, label='Nyquist Plot')
+plt.plot(Re_FRF/(natural_omega_peaks)**2, Im_FRF/(natural_omega_peaks)**2, label='Nyquist Plot')
 # plt.plot(Re_FRF, -Im_FRF, linestyle='--', label='Conjugate symmetry')
 
 # Ajout des axes pour plus de clarté
