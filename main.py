e7d87ed9458073abcd7923f06c77373d17b0e593
emilendlbb <emilien.delabrassinnebonardeaux@student.uliege.be>
2024-10-28 10:08:57 +0100

    toute la première partie de la partie3

--- main.py
+++ main.py
@@ -3,182 +3,267 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 from scipy.integrate import cumulative_trapezoid
+import sympy as sp
 
-current_dir = os.path.dirname(__file__)
 
-freq_path = os.path.join(current_dir, 'files', 'P2024_frf_acc.txt')
-acc_path = os.path.join(current_dir, 'files', 'P2024_irf_acc.txt')
+def partie2():
+    current_dir = os.path.dirname(__file__)
 
-data_freq = np.loadtxt(freq_path)
-freq = data_freq[:, 0]
-omega_squared = (2 * np.pi * freq) ** 2
-Re_FRF = data_freq[:, 1] / omega_squared
-Im_FRF = data_freq[:, 2] / omega_squared
-#Vu qu'on divise par 0 pour le premier élément, on a un nan à l'indice 0,
-#Mise à 0 pour les graphiques
-Re_FRF[0] = Re_FRF[1]
-Im_FRF[0] = Im_FRF[1]
-data_acc = np.loadtxt(acc_path)
-time = data_acc[:, 0]
-acc = data_acc[:, 1]
+    freq_path = os.path.join(current_dir, 'files', 'P2024_frf_acc.txt')
+    acc_path = os.path.join(current_dir, 'files', 'P2024_irf_acc.txt')
 
-def damped_natural_frequency(data_acc):
+    data_freq = np.loadtxt(freq_path)
+    freq = data_freq[:, 0]
+    omega_squared = (2 * np.pi * freq) ** 2
+    Re_FRF = - data_freq[:, 1] / omega_squared
+    Im_FRF = - data_freq[:, 2] / omega_squared
+    #Vu qu'on divise par 0 pour le premier élément, on a un nan à l'indice 0,
+    #Mise à aux éléments 1 pour les graphiques
+    Re_FRF[0] = Re_FRF[1]
+    Im_FRF[0] = Im_FRF[1]
+    data_acc = np.loadtxt(acc_path)
     time = data_acc[:, 0]
     acc = data_acc[:, 1]
 
-    peaks, _ = find_peaks(acc)
+    def damped_natural_frequency(data_acc):
+        time = data_acc[:, 0]
+        acc = data_acc[:, 1]
 
-    time_peaks = time[peaks]
+        peaks, _ = find_peaks(acc)
 
-    time_peaks_difference = np.diff(time_peaks)
+        time_peaks = time[peaks]
 
-    time_peaks_difference = time_peaks_difference[7:]
+        time_peaks_difference = np.diff(time_peaks)
 
-    avg_time_peaks_difference = np.mean(time_peaks_difference)
+        time_peaks_difference = time_peaks_difference[7:]
 
-    damped_natural_frequency = 1/avg_time_peaks_difference
+        avg_time_peaks_difference = np.mean(time_peaks_difference)
 
-    return damped_natural_frequency
+        damped_natural_frequency = 1/avg_time_peaks_difference
 
-
-def integrate_acc(data_acc):
-    time = data_acc[:, 0]
-    acc = data_acc[:, 1]
-
-    velocity = cumulative_trapezoid(acc, time, initial=0)
-
-    displacement = cumulative_trapezoid(velocity, time, initial=0)
-
-    return displacement, velocity
-
-def log_method(displacement):
-    peaks, _ = find_peaks(displacement)
-
-    displacement_peaks = displacement[peaks]
-    # print(displacement_peaks)
-
-    displacement_peaks = displacement_peaks[1:6]
-    # print(displacement_peaks)
-
-    log_quotient = np.log(np.abs(displacement_peaks[:-1] / displacement_peaks[1:]))
-    # print(log_quotient)
-
-    mean_log = np.mean(log_quotient)
-
-    damping_ratio = mean_log / (2 * np.pi)
-
-    return damping_ratio
-
-    
+        return damped_natural_frequency
 
 
-#methode avec l'accélération
+    def integrate_acc(data_acc):
+        time = data_acc[:, 0]
+        acc = data_acc[:, 1]
 
-natural_freq_peaks = damped_natural_frequency(data_acc)
-print(f"Estimated natural frequency from peaks: {natural_freq_peaks} Hz")
+        velocity = cumulative_trapezoid(acc, time, initial=0)
 
-natural_omega_peaks = 2*np.pi*natural_freq_peaks
-print(f"Estimated natural pulsation from peaks: {natural_omega_peaks} rad/s")
+        displacement = cumulative_trapezoid(velocity, time, initial=0)
 
-displacement, velocity = integrate_acc(data_acc)
-# print(displacement)
+        return displacement, velocity
 
-damping_ratio_log_method = log_method(displacement)
-print(f"Estimated damping ratio from log method: {damping_ratio_log_method}")
+    def log_method(displacement):
+        peaks, _ = find_peaks(displacement)
 
-plt.plot(time, acc)
-plt.show()
+        displacement_peaks = displacement[peaks]
+        # print(displacement_peaks)
 
+        displacement_peaks = displacement_peaks[1:6]
+        # print(displacement_peaks)
 
+        log_quotient = np.log(np.abs(displacement_peaks[:-1] / displacement_peaks[1:]))
+        # print(log_quotient)
 
+        mean_log = np.mean(log_quotient)
 
+        damping_ratio_approx = mean_log / (2 * np.pi)
 
-#méthode avec diagramme de Bode
+        x = sp.symbols('x')
 
-fonction_transfert = (Re_FRF + 1j * Im_FRF)
+        # Définir l'équation
+        equation = sp.Eq(mean_log, (2 * sp.pi * x) / sp.sqrt(1 - x**2))
 
-# Calculer l'amplitude (module) et la phase (argument)
-amplitude = np.abs(fonction_transfert)
-phase = np.angle(fonction_transfert)
+        # Résoudre l'équation pour x
+        damping_ratio_exact = sp.solve(equation, x)
 
-pi_over_2_phase_index = np.argmin(np.abs(np.abs(phase)-np.pi/2))
-natural_frequency_bode = freq[pi_over_2_phase_index]
+        return damping_ratio_approx, damping_ratio_exact
 
-peak_bode = np.argmax(amplitude)
-max_amplitude_frequency_bode = freq[peak_bode]
-
-print(f"Estimated natural frequency from Bode plot: {natural_frequency_bode} Hz")
-print(f"Maximum amplitude frequency from Bode plot: {max_amplitude_frequency_bode} Hz")
-
-plt.figure()
-
-# Amplitude
-plt.subplot(2, 1, 1)
-plt.plot(freq, amplitude, label="Amplitude")
-plt.axvline(x=max_amplitude_frequency_bode, color='r', linestyle='--', label=f"Max Amplitude @ {max_amplitude_frequency_bode:.2f} Hz")
-plt.axvline(x=natural_frequency_bode, color='g', linestyle='--', label=f"Phase = pi/2 @ {natural_frequency_bode:.2f} Hz")
-plt.title("Amplitude de la fonction de transfert")
-plt.xlabel("Fréquence (Hz)")
-plt.ylabel("Amplitude")
-
-# Phase
-plt.subplot(2, 1, 2)
-plt.plot(freq, phase, label="Phase")
-plt.axvline(x=natural_frequency_bode, color='g', linestyle='--', label=f"Phase = pi/2 @ {natural_frequency_bode:.2f} Hz")
-plt.xlim(left=freq[0] - 4)
-xmin, xmax = plt.gca().get_xlim()
-plt.plot([xmin, natural_frequency_bode], [np.pi/2, np.pi/2], 'g--', label=f"Phase = pi/2")
-plt.yticks([0, np.pi/4, np.pi/2, 3*np.pi/4, np.pi], [r'$0$', r'$\frac{\pi}{4}$', r'$\frac{\pi}{2}$', r'$\frac{3\pi}{4}$', r'$\pi$'])
-plt.title("Phase de la fonction de transfert")
-plt.xlabel("Fréquence (Hz)")
-plt.ylabel("Phase (radians)")
-
-plt.tight_layout()
-plt.show()
+        
 
 
-#half-power method
+    #methode avec l'accélération
 
-half_power_amplitude = amplitude[peak_bode] / np.sqrt(2)
-half_power_indices = np.where(amplitude >= half_power_amplitude)[0]
-bandwidth_freqs = freq[half_power_indices]
+    natural_freq_peaks = damped_natural_frequency(data_acc)
+    print(f"Estimated natural frequency from peaks: {natural_freq_peaks} Hz")
 
-delta_f = bandwidth_freqs[-1] - bandwidth_freqs[0]
-print(f"Bandwidth Δf: {delta_f} Hz")
+    natural_omega_peaks = 2*np.pi*natural_freq_peaks
+    print(f"Estimated natural pulsation from peaks: {natural_omega_peaks} rad/s")
 
-Q_factor = natural_frequency_bode / delta_f
-print(f"Estimated Quality Factor (Q): {Q_factor}")
+    displacement, velocity = integrate_acc(data_acc)
+    # print(displacement)
 
-damping_ratio_half_power = 1 / (2 * Q_factor)
-print(f"Estimated damping ratio using half-power method: {damping_ratio_half_power}")
+    damping_ratio_log_method_approx, damping_ratio_log_method_exact = log_method(displacement)
+    print(f"Estimated damping ratio from log method: {damping_ratio_log_method_approx}")
+    print(f"Exact damping ratio from log method: {damping_ratio_log_method_exact}")
+    print(f"Relative error from log method: {(damping_ratio_log_method_approx - damping_ratio_log_method_exact)/damping_ratio_log_method_exact}")
+
+
+    plt.plot(time, acc)
+    plt.grid()
+    plt.show()
+    plt.plot(time, velocity)
+    plt.grid()
+    plt.show()
+    plt.plot(time, displacement)
+    plt.grid()
+    plt.show()
 
 
 
-natural_frequency_nyquist = freq[102]
-print(f"Estimated natural frequency from Nyquist plot: {natural_frequency_nyquist} Hz")
 
-nyquist_amplitude_natural_frequency = np.sqrt(Im_FRF[102]**2 + Re_FRF[102]**2)
-equivalent_mass = 87.5
-damping_ratio_nyquist = 1/(2*equivalent_mass*nyquist_amplitude_natural_frequency)
-print(f"Estimated damping ratio using Nyquist plot: {damping_ratio_nyquist}")
 
-#nyquist
-# Tracer le diagramme de Nyquist
-plt.figure()
+    #méthode avec diagramme de Bode
 
-plt.plot(Re_FRF, Im_FRF, label='Nyquist Plot')
-# plt.plot(Re_FRF, -Im_FRF, linestyle='--', label='Conjugate symmetry')
+    fonction_transfert = (Re_FRF + 1j * Im_FRF)
 
-# Ajout des axes pour plus de clarté
-plt.axhline(0, color='black',linewidth=0.5)
-plt.axvline(0, color='black',linewidth=0.5)
+    # Calculer l'amplitude (module) et la phase (argument)
+    amplitude = np.abs(fonction_transfert)
+    phase = np.angle(fonction_transfert)
 
-# Titres et légendes
-plt.title("Diagramme de Nyquist")
-plt.xlabel("Partie réelle")
-plt.ylabel("Partie imaginaire")
-plt.legend()
+    pi_over_2_phase_index = np.argmin(np.abs(np.abs(phase)-np.pi/2))
+    natural_frequency_bode = freq[pi_over_2_phase_index]
 
-plt.grid(True)
-plt.axis('equal')  # Pour avoir des échelles identiques sur les deux axes
-plt.show()
+    peak_bode = np.argmax(amplitude)
+    max_amplitude_frequency_bode = freq[peak_bode]
+
+    print(f"Estimated natural frequency from Bode plot: {natural_frequency_bode} Hz")
+    print(f"Maximum amplitude frequency from Bode plot: {max_amplitude_frequency_bode} Hz")
+
+    plt.figure(figsize=(10, 8))
+
+
+    amplitude_dB = 20*np.log10(amplitude)
+    max_amplitude_dB = np.max(amplitude_dB)
+    half_power_amplitude_dB = max_amplitude_dB - 3  # Demi-puissance en dB
+
+    half_power_indices = np.where(amplitude_dB >= half_power_amplitude_dB)[0]
+    bandwidth_freqs = freq[half_power_indices]
+    half_power_frequencies = (bandwidth_freqs[0], bandwidth_freqs[-1])
+    min_amplitude_dB = np.min(amplitude_dB)
+    natural_frequency_dB = amplitude_dB[np.argmin(np.abs(freq - natural_frequency_bode))]
+
+    # Amplitude
+    plt.subplot(2, 1, 1)
+    plt.grid()
+
+    # Configurer les graduations de l'axe y avec un intervalle de 10 dB
+    y_min = np.floor(np.min(amplitude_dB) / 5) * 5
+    y_max = np.ceil(np.max(amplitude_dB) / 5) * 5
+    plt.yticks(np.arange(y_min, y_max + 1, 5))  # Intervalle de 10 dB
+
+    plt.ylim([np.min(amplitude_dB) - 2, max_amplitude_dB + 2])
+    x_min, x_max = plt.gca().get_xlim()
+    y_min, y_max = plt.gca().get_ylim()
+
+    plt.plot(freq, amplitude_dB, label="Amplitude (dB)")
+    plt.plot([max_amplitude_frequency_bode, max_amplitude_frequency_bode], [y_min, max_amplitude_dB], 'r--')
+    plt.plot([x_min, max_amplitude_frequency_bode], [max_amplitude_dB, max_amplitude_dB], 'r--')
+    plt.plot(max_amplitude_frequency_bode, max_amplitude_dB, 'ro')
+    plt.plot([x_min, half_power_frequencies[1]], [half_power_amplitude_dB, half_power_amplitude_dB], 'b--')
+    plt.plot([half_power_frequencies[0], half_power_frequencies[0]], [y_min, half_power_amplitude_dB], 'b--')
+    plt.plot([half_power_frequencies[1], half_power_frequencies[1]], [y_min, half_power_amplitude_dB], 'b--')
+    plt.plot(half_power_frequencies[0], half_power_amplitude_dB, 'bo')
+    plt.plot(half_power_frequencies[1], half_power_amplitude_dB, 'bo')
+    plt.plot([natural_frequency_bode, natural_frequency_bode], [y_min, max_amplitude_dB - 0.4], 'g--')
+    plt.plot(natural_frequency_bode, max_amplitude_dB - 0.4, 'go')
+    plt.annotate(r'$\frac{{X}_{max}}{\sqrt{2}}$', xy=(max_amplitude_frequency_bode-90, half_power_amplitude_dB), color='blue')
+    plt.annotate(r'${X}_{max}$', xy=(max_amplitude_frequency_bode-90, max_amplitude_dB), color='red')
+    plt.annotate(r'$f_a$', xy=(max_amplitude_frequency_bode + 1.5, max_amplitude_dB-18), color='red')
+    mid_freq = (half_power_frequencies[0] + half_power_frequencies[1]) / 2
+    plt.annotate(r'$\Delta f$', xy=(mid_freq, half_power_amplitude_dB - 11), color='blue', ha='center')
+
+    plt.annotate('', 
+                xy=(half_power_frequencies[0], half_power_amplitude_dB - 12), 
+                xytext=(half_power_frequencies[1], half_power_amplitude_dB - 12),
+                arrowprops=dict(arrowstyle='<->', color='blue'))
+
+    plt.title("Amplitude de la fonction de transfert")
+    plt.xlabel("Fréquence (Hz)")
+    plt.ylabel("Amplitude (dB)")
+
+    # Phase
+    plt.subplot(2, 1, 2)
+    plt.grid()
+    x_min, x_max = plt.gca().get_xlim()
+    y_min, y_max = plt.gca().get_ylim()
+    plt.plot(freq, phase + np.pi, label="Phase")
+    plt.xlim(left=freq[0] - 4)
+    plt.plot([natural_frequency_bode, natural_frequency_bode], [y_min, np.pi/2], 'g--')
+    plt.plot([x_min - 4, natural_frequency_bode], [np.pi/2, np.pi/2], 'g--')
+    plt.plot(natural_frequency_bode, np.pi/2, 'go')
+    plt.annotate(r'$f_0$', xy=(natural_frequency_bode + 1.5, y_min), color='green')
+    plt.yticks([0, np.pi/4, np.pi/2, 3*np.pi/4, np.pi], [r'$-\pi$', r'$-\frac{3\pi}{4}$', r'$-\frac{\pi}{2}$', r'$-\frac{\pi}{4}$', r'$0$'])
+    plt.title("Phase de la fonction de transfert")
+    plt.xlabel("Fréquence (Hz)")
+    plt.ylabel("Phase (radians)")
+
+
+    plt.tight_layout()
+    plt.show()
+
+
+    #half-power method
+
+    half_power_amplitude = amplitude[peak_bode] / np.sqrt(2)
+    half_power_indices = np.where(amplitude >= half_power_amplitude)[0]
+    bandwidth_freqs = freq[half_power_indices]
+
+    delta_f = bandwidth_freqs[-1] - bandwidth_freqs[0]
+    print(f"Bandwidth Δf: {delta_f} Hz")
+
+    Q_factor = natural_frequency_bode / delta_f
+    print(f"Estimated Quality Factor (Q): {Q_factor}")
+
+    damping_ratio_half_power = 1 / (2 * Q_factor)
+    print(f"Estimated damping ratio using half-power method: {damping_ratio_half_power}")
+
+
+
+    natural_frequency_nyquist = freq[102]
+    print(f"Estimated natural frequency from Nyquist plot: {natural_frequency_nyquist} Hz")
+
+
+    # Nyquist en accélération
+    # Re_FRF = data_freq[:, 1]
+    # Im_FRF = data_freq[:, 2]
+
+    # nyquist_amplitude_natural_frequency = np.sqrt(Im_FRF[102]**2 + Re_FRF[102]**2)
+    # equivalent_mass = 87.5
+    # damping_ratio_nyquist = 1/(2*equivalent_mass*nyquist_amplitude_natural_frequency)
+    # print(f"Estimated damping ratio using Nyquist plot: {damping_ratio_nyquist}")
+
+    # Nyquist en déplacement
+    nyquist_amplitude_natural_frequency = np.sqrt(Im_FRF[102]**2 + Re_FRF[102]**2)
+    equivalent_mass = 87.5
+    damping_ratio_nyquist = 1/(2*equivalent_mass*nyquist_amplitude_natural_frequency*(2*np.pi*natural_frequency_nyquist)**2)
+    print(f"Estimated damping ratio using Nyquist plot: {damping_ratio_nyquist}")
+
+    #nyquist
+    # Tracer le diagramme de Nyquist
+    plt.figure()
+
+    plt.plot(Re_FRF, Im_FRF, label='Nyquist Plot')
+    plt.plot(Re_FRF[102], Im_FRF[102], 'ro')
+    # plt.plot(Re_FRF, -Im_FRF, linestyle='--', label='Conjugate symmetry')
+    plt.annotate("A", (Re_FRF[102], Im_FRF[102]), textcoords="offset points", xytext=(5,5), ha='center', color='red')
+
+    # Ajout des axes pour plus de clarté
+    plt.axhline(0, color='black',linewidth=0.5)
+    plt.axvline(0, color='black',linewidth=0.5)
+
+    # Titres et légendes
+    plt.title("Diagramme de Nyquist")
+    plt.xlabel("Partie réelle")
+    plt.ylabel("Partie imaginaire")
+    plt.legend()
+
+    plt.grid(True)
+    plt.axis('equal')  # Pour avoir des échelles identiques sur les deux axes
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    partie2()
---
+++ main_partie3.py
@@ -0,0 +1,157 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
+from scipy.integrate import cumulative_trapezoid
+import sympy as sp
+
+def load_data():
+    current_dir = os.path.dirname(__file__)
+
+    frf_path = os.path.join(current_dir, 'files', 'P2024_frf_Part3_f_ds.txt')
+    modes_path = os.path.join(current_dir, 'files', 'P2024_modes_Part3.txt')
+    damping_path = os.path.join(current_dir, 'files', 'P2024_f_eps_Part3.txt')
+
+    frf_data = np.loadtxt(frf_path)
+    modes_data = np.loadtxt(modes_path)
+    damping_data = np.loadtxt(damping_path)
+
+    natural_freq = damping_data[:, 0]
+    damping_ratios = damping_data[:, 1]
+
+    freq_frf = frf_data[:, 0]
+    Re_frf = frf_data[:, 1]
+    Im_frf = frf_data[:, 2]
+
+    mode1 = modes_data[:, 0]
+    mode2 = modes_data[:, 1]
+    mode3 = modes_data[:, 2]
+    mode4 = modes_data[:, 3]
+
+    return natural_freq, damping_ratios, freq_frf, Re_frf, Im_frf, modes_data, mode1, mode2, mode3, mode4
+
+def plot_excitation_force(F_0, wavelenght, speed, time_interval, sampling_rate=10000):
+    Omega = speed / wavelenght * 2* np.pi
+
+    plt.figure()
+
+    time = np.linspace(0, time_interval, (int)(sampling_rate*time_interval))
+    F_z = F_0*np.sin(Omega*time)
+    plt.plot(time, F_z)
+
+    plt.title("Force excitatrice")
+    plt.xlabel("Temps [s]")
+    plt.ylabel("Force [N]")
+
+    plt.grid(True)
+    plt.show()
+
+    plt.show()   
+
+def compute_FRF_matrix(natural_frequencies, damping_ratios, modes, frequencies_range):
+    nb_modes = modes.shape[1]
+    nb_points = modes.shape[0]
+    nb_freq = len(frequencies_range)
+
+    FRF_matrix = np.zeros((nb_points, nb_points, nb_freq), dtype=complex)
+
+    masses = np.ones(nb_modes)  # les modes sont normalisés du point de vue de la masse
+    raideurs = (2*np.pi*natural_frequencies)**2 * masses
+
+    for i, freq in enumerate(frequencies_range):
+        frf = np.zeros((nb_points, nb_points), dtype=complex)
+        for mode in range(nb_modes):
+            omega = 2*np.pi*freq
+            omega_r = 2*np.pi*natural_frequencies[mode]
+            mode_vector = modes[:, mode]
+            numerator = np.outer(mode_vector, mode_vector)
+            denominator = raideurs[mode] - masses[mode]*omega**2 + 2 * 1j * masses[mode] * damping_ratios[mode] * omega_r * omega
+            frf += -omega**2 * numerator/denominator
+        FRF_matrix[:, :, i] = frf
+
+    return FRF_matrix
+
+def plot_Bode_Nyquist(FRF_matrix, frequencies_range, first_point_index, second_point_index, freq_frf, Re_frf, Im_frf):
+    plt.figure()
+
+    amplitude_FRF = np.abs(FRF_matrix[first_point_index, second_point_index, :])
+    fonction_de_transfert = Re_frf + 1j * Im_frf
+    amplitude_data = np.abs(fonction_de_transfert)
+
+    # Bode
+    plt.subplot(2, 1, 1)
+    plt.semilogx(frequencies_range, 20 * np.log10(amplitude_FRF), label='FRF_matrix')
+    plt.semilogx(freq_frf, 20 * np.log10(amplitude_data), label='Data', linestyle='--')
+    plt.title('Bode Amplitude')
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Amplitude (dB)')
+    plt.legend()
+    plt.grid()
+
+    # Nyquist
+    plt.subplot(2, 1, 2)
+    nyquist_real_FRF = np.real(FRF_matrix[first_point_index, second_point_index, :])
+    nyquist_imag_FRF = np.imag(FRF_matrix[first_point_index, second_point_index, :])
+    plt.plot(nyquist_real_FRF, nyquist_imag_FRF, label='FRF_matrix')
+    plt.plot(Re_frf, Im_frf, label='Data', linestyle='--')
+
+    plt.title('Nyquist')
+    plt.xlabel('Real Part')
+    plt.ylabel('Imaginary Part')
+    plt.axhline(0, color='grey', lw=0.5, ls='--')
+    plt.axvline(0, color='grey', lw=0.5, ls='--')
+    plt.grid()
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+def max_amplitude(FRF_matrix, frequencies_range, F_0, wavelenght, speed, time_interval, sampling_rate=10000):
+    Omega = speed / wavelenght * 2* np.pi
+
+    time = np.linspace(0, time_interval, (int)(sampling_rate*time_interval))
+    F_z = F_0*np.sin(Omega*time)
+
+    # Fourier
+    F_w = np.array  ([np.sum(F_z * np.exp(-1j * 2 * np.pi * f * time)) for f in frequencies_range])
+
+    max_amplitudes = np.zeros(FRF_matrix.shape[0])
+
+    for i in range(FRF_matrix.shape[0]):
+        X_w = FRF_matrix[i, 0, :] * F_w
+        max_amplitudes[i] = np.max(np.abs(X_w))
+
+    return max_amplitudes
+
+def plot_max_amplitude(max_amplitudes):
+    plt.figure()
+    plt.plot(range(len(max_amplitudes)), max_amplitudes, 'o', color='red')
+    plt.xlabel("Accéléromètre indice")
+    plt.ylabel(r"$\max |X_{i,0}(\omega)|$")
+    plt.title("Réponse maximum en fréquentiel")
+    plt.grid(True)
+    plt.show()
+
+if __name__ == '__main__':
+    natural_freq, damping_ratios, freq_frf, Re_frf, Im_frf, modes, mode1, mode2, mode3, mode4 = load_data()
+    modes = modes/1000      # m
+
+    speed = 50/3.6          # m/s
+    wavelenght = 0.2        # m
+    F_0 = 450               # N
+
+    point_distance = np.linspace(0, 1200, 13)
+    point_distance = np.insert(point_distance, 5, 400)
+
+    time_interval = 0.15    # s
+    # plot_excitation_force(F_0, wavelenght, speed, time_interval)
+
+    # crée une matrice 14*14*nb_freq
+    FRF_matrix = compute_FRF_matrix(natural_freq, damping_ratios, modes, freq_frf)
+
+    # plot_Bode_Nyquist(FRF_matrix, freq_frf, 0, 11, freq_frf, Re_frf, Im_frf)
+
+    max_amplitudes = max_amplitude(FRF_matrix, freq_frf, F_0, wavelenght, speed, time_interval)
+
+    plot_max_amplitude(max_amplitudes)
+
+        
